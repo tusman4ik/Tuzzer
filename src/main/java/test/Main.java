de4173ebe4615085bf6135091bee0450dcaa3673
tusman4ik <tusman4ik@gmail.com>
2024-06-14 22:22:56 +0300

    Bugfix:

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,7 +1,7 @@
 package test;
 
 import lombok.extern.slf4j.Slf4j;
-import org.example.Test;
+import org.example.Example;
 import org.example.utils.Tuzzer;
 import org.example.generators.IntGenerator;
 import org.example.limiters.Limiter;
@@ -10,7 +10,7 @@ import org.example.reporters.LogReporter;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        Tuzzer.of(new Test(),"dev")
+        Tuzzer.of(new Example(),"dev")
                 .withArg(int.class, new IntGenerator().generateCustomBorderLineValue(123))
                 .withArg(int.class, new IntGenerator().generateCustomBorderLineValue(123))
                 .withArg(int.class, new IntGenerator().generateCustomBorderLineValue(123))
